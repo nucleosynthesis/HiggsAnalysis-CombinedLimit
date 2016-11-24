da37e0d76388d0562ff3c3fb5405e2fa3dd51297
nckw <n.c.k.wardle@googlemail.com>
2016-11-24 13:19:22 +0100

    Update checkout.sh

--- checkout.sh
+++ checkout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-set -x
-set -e
+#set -x
+#set -e
 git clone -b combine_tutorial_SWAN https://github.com/nucleosynthesis/HiggsAnalysis-CombinedLimit HiggsAnalysis/CombinedLimit
 cd HiggsAnalysis/CombinedLimit
 source env_standalone_make.sh
