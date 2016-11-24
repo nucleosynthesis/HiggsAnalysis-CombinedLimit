129aa1e15decb9e690510ce858a3e21914e32f56
Nicholas Wardle <Nick@nick-mac.dyndns.cern.ch>
2016-11-24 13:08:37 +0100

    Adding checkout

---
+++ checkout.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+set -e
+git clone -b combine_tutorial_SWAN https://github.com/nucleosynthesis/HiggsAnalysis-CombinedLimit HiggsAnalysis/CombinedLimit
+cd HiggsAnalysis/CombinedLimit
+source env_standalone_make.sh
