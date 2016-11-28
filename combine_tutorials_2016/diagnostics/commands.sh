b192038aa428853ccbd2e05cef317979c5a4fc1d
Nicholas Wardle <Nick@nick-mac.dyndns.cern.ch>
2016-11-28 20:39:38 +0100

    5000->200

--- combine_tutorials_2016/diagnostics/commands.sh
+++ combine_tutorials_2016/diagnostics/commands.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 text2workspace.py ttH_hbb_13TeV_sl_j5_tge4_high.txt
+
+# 2 fits to the data
 combine ttH_hbb_13TeV_sl_j5_tge4_high.root -M MaxLikelihoodFit --rMin -5 --rMax 5  
-combine ttH_hbb_13TeV_sl_j5_tge4_high.root -M MaxLikelihoodFit --toysFrequentist –t 5000 --rMin -5 --rMax 5 --expectSignal 0 –n toys
+
+# run the same with toys 
+combine ttH_hbb_13TeV_sl_j5_tge4_high.root -M MaxLikelihoodFit --toysFrequentist –t 200 --rMin -5 --rMax 5 --expectSignal 0 –n toys
