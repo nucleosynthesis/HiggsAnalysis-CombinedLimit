6b1edda1e920bcec5005d4e5024788a50d1529c4
Nicholas Wardle <Nick@nick-mac.dyndns.cern.ch>
2016-11-28 19:21:43 +0100

    Add tutorial for scaling nuisances

---
+++ combine_tutorials_2016/parametric_histograms/commands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+combineCards.py control_region_scaled.txt signal_region_scaled.txt > combined_card.txt  
+
+text2workspace.py combined_card.txt --X-nuisance-function '.*' 'expr::lumisyst("1/sqrt(@0)",luminosity[1])' -o all_scaled.root
+
+# scale for different luminosities 
+
+
+combine all_scaled.root -M Asymptotic --noFitAsimov --setPhysicsModelParameters luminosity=1  --trackParameters luminosity -n allscaled_1
+
+combine all_scaled.root -M Asymptotic --noFitAsimov --setPhysicsModelParameters luminosity=10  --trackParameters luminosity -n allscaled_10
+
+
+combine all_scaled.root -M Asymptotic --noFitAsimov --setPhysicsModelParameters luminosity=50  --trackParameters luminosity -n allscaled_50
+
+
+combine all_scaled.root -M Asymptotic --noFitAsimov --setPhysicsModelParameters luminosity=100  --trackParameters luminosity -n allscaled_100
+
+hadd -f lims_all_scaled.root higgsCombineallscaled_*.root
