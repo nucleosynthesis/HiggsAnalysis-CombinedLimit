7197a3f34eaf504c119936c7c1597be3afb99fe3
nckw <n.c.k.wardle@googlemail.com>
2016-11-30 11:48:24 +0100

    Update env_standalone_make.sh

--- env_standalone_make.sh
+++ env_standalone_make.sh
@@ -1,10 +1,4 @@
 export PATH=${PATH}:${PWD}/exe:${PWD}/scripts
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/lib
 export PYTHONPATH=${PYTHONPATH}:${PWD}/lib/python:${PWD}/lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWDX}:/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan3/vdt/0.3.6/x86_64-slc6-gcc49-opt/lib 
-
-# Run a second time seems to be needed ?
-export PATH=${PATH}:${PWD}/exe:${PWD}/scripts
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/lib
-export PYTHONPATH=${PYTHONPATH}:${PWD}/lib/python:${PWD}/lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWDX}:/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan3/vdt/0.3.6/x86_64-slc6-gcc49-opt/lib 
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}:/cvmfs/sft.cern.ch/lcg/releases/LCG_85swan3/vdt/0.3.6/x86_64-slc6-gcc49-opt/lib 
